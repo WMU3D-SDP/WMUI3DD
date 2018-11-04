332c6a9a155cdd2a99beda1fb5eccffaabf103fc
WMU3D-SDP <jeff.d.howe@wmich.edu>
2018-11-04 12:31:51 -0500

    add tourdata.txt

---
+++ upload.php
@@ -0,0 +1,9 @@
+<?php
+
+shell_exec('git add --all');
+shell_exec('git commit -am "add  tourdata.txt"');
+shell_exec('git push -u origin master');
+shell_exec('WMU3D-SDP');
+shell_exec('wmu3d2018');
+
+?>
