8c811320a6652d8975f2654a24ba2f50c15141ad
WMU3D-SDP <jeff.d.howe@wmich.edu>
2018-11-04 12:40:52 -0500

    something

--- upload.php
+++ upload.php
@@ -3,7 +3,5 @@
 shell_exec('git add --all');
 shell_exec('git commit -am "add  tourdata.txt"');
 shell_exec('git push -u origin master');
-shell_exec('echo WMU3D-SDP');
-shell_exec('echo wmu3d2018');
 
 ?>
