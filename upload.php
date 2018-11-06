fdd6aeafd2b642be019a66acaa92d5b5911371f1
WMU3D-SDP <jeff.d.howe@wmich.edu>
2018-11-06 14:29:54 -0500

    update tourdata

--- upload.php
+++ upload.php
@@ -1,8 +1,9 @@
 <?php
-echo shell_exec('git config --global --get user.email');
-echo shell_exec('git config --global --get user.name');
+
+//echo shell_exec('git config --global --get user.email');
+//echo shell_exec('git config --global --get user.name');
 echo shell_exec('git add --all');
-echo shell_exec('git commit -m "update tourdata"');
-echo shell_exec('git push origin master');
+echo shell_exec('sudo -u pi git commit -m "update tourdata"');
+//echo shell_exec('git push origin master');
 
 ?>
