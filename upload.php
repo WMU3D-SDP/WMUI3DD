84ca2e7d6aae20c9cc4d1a59097aea1a10166e04
WMU3D-SDP <jeff.d.howe@wmich.edu>
2018-11-06 18:21:14 -0500

    update tourdata

--- upload.php
+++ upload.php
@@ -3,5 +3,8 @@
 echo shell_exec('git add --all');
 echo shell_exec('sudo -u pi git commit -m "update tourdata"');
 echo shell_exec('sudo -u pi git push origin master');
+echo shell_exec('sudo -u pi git mv tourdata.txt date("Y_m_d")');
+echo shell_exec('sudo -u pi git commit -m "rename"');
+echo shell_exec('sudo -u pi push origin master');
 
 ?>
