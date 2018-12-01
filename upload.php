6ca87cb3ae9449255fd9c9f9f817dd5f8e7c0803
WMU3D-SDP <jeff.d.howe@wmich.edu>
2018-12-01 14:31:59 -0500

    update tourdata

--- upload.php
+++ upload.php
@@ -8,7 +8,7 @@ $date = new DateTime();
 rename("tourdata.txt" , "tourdata" . $date->format('Y_m_d_H:i') . ".txt");
 */
 echo shell_exec('sudo -u root git add --all');
-echo shell_exec('sudo -u pi git commit -m "update tourdata"');
+echo shell_exec('sudo -u root git commit -m "update tourdata"');
 echo shell_exec('sudo -u pi git push origin master');
 
 ?>
